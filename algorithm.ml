7bffc19cd44185a0e6380fd1c4635194dd5a9c15
Marc Blanchard <marc.blanchard600@gmail.com>
2020-11-12 19:52:41 +0100

    ok ca marche pas on travaille dessus

--- algorithm.ml
+++ algorithm.ml
@@ -2,36 +2,36 @@
  * algorithm that can be called by other modules
 .*)
 
-type point = {x : float; y : float} (* définit dans géometry et à importer si besoin *)
+type point = {x : float; y : float};; (* définit dans géometry et à importer si besoin *)
 
-type particule = point list
+
 
 (* les points mde départ et d'arrivée' *)
-let P_init = (0., 0.)     
-let P_final = (100., 100.)
+let p_init = (0., 0.);;     
+let p_final = (100., 100.);;
 
 (* le nombre de points tournant *)
-let nb_pt = 3
+let nb_pt = 3;;
 
 
 (* sert pour test fonction de génération, à remplacer par une génération aléatoire *)
-let randx = 5.
-let randy = 6.
+let randx = 5.;;
+let randy = 6.;;
 
-let generation_particule = fun NB_PT ->
+(*let generation_particule = fun max ->
 	let particule = [] in
-	for ident = 1 to NB_PT do
-		let new_point = [x = randx; y = randy] in                                  (* borner l'ensemble de genération d'un point en fonction du précedent pour eviter un retour en arrière trop important *)
-		List.append particule new_point
-	done
+	for ident = 1 to max do
+		let new_point = [{x = randx; y = randy}] in                                  (* borner l'ensemble de genération d'un point en fonction du précedent pour eviter un retour en arrière trop important *)
+		new_point :: particule;
+	done;;*)
 
-let generation = fun NB_PT->
+let generation = fun max->
 	let rec rec_gen = fun particule increment ->
-		if increment = NB_PT then particule 
+		if increment = max then particule 
 		else
-			rec_generation (List.append particule [x = randx; y = randy]) increment+1
-	rec_gen [] 0
+			rec_gen ({x = randx; y = randy} :: particule) (increment+1)
+	in rec_gen [] 0;;	
 
-
-let particule1 = generation_particule nb_pt
-let particule2 = generation nb_pt
+(*let particule1 = generation_particule nb_pt;*)
+let particule2 = generation nb_pt;;
+Printf.fprintf (string particule2);;
