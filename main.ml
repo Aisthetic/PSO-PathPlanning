e2189efe390f9fb4e8a0b8c19537218b4e59fb8c
Aisthetic <zakaria.ezzahed@hotmail.fr>
2020-11-13 12:20:53 +0100

    added interactive loop

--- main.ml
+++ main.ml
@@ -1,6 +1,11 @@
 open Graphics;;
+let rec interactive () =
+  let event = wait_next_event [Key_pressed] in
+  if event.key == 'q' then exit 0
+  else print_char event.key; print_newline (); interactive ();;
 Graphics.open_graph " 200x200";;
 let f x = x + 1;;
 for i = 0 to 200 do
   plot i (f i)
 done;;
+interactive ();;
