9c5e2bb0f15d8cdc4718453aa70663609ccafaf1
Aisthetic <zakaria.ezzahed@hotmail.fr>
2020-11-25 19:47:26 +0100

    added scaling for user interface

--- main.ml
+++ main.ml
@@ -1,19 +1,18 @@
-
 open Gui
 
-let pt1 = {x = 200.; y = 200.};;
-let pt2 = {x = 100.; y = 200.};;
-let pt3 = {x = 200.; y = 100.};;
+let pt1 = {x = 20.; y = 20.};;
+let pt2 = {x = 10.; y = 20.};;
+let pt3 = {x = 20.; y = 10.};;
 let polyg = [| pt1; pt2; pt3 |];;
 
-let pt11 = {x = 133.; y = 0.};;
-let pt21 = {x = 0.; y = 111.};;
-let pt31 = {x = 133.; y = 133.};;
+let pt11 = {x = 13.; y = 0.};;
+let pt21 = {x = 0.; y = 11.};;
+let pt31 = {x = 13.; y = 13.};;
 let polyg1 = [| pt11; pt21; pt31 |];;
 
 let pt111 = {x = 0.; y = 0.};;
-let pt211 = {x = 100.; y = 111.};;
-let pt311 = {x = 133.; y = 200.};;
+let pt211 = {x = 10.; y = 11.};;
+let pt311 = {x = 13.; y = 20.};;
 
 let path = [| pt111; pt211; pt311 |];;
 
