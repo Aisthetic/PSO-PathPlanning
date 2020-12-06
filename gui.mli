efce011d87a4711a5c7a14bbfdaa31c9fc69a404
SElpida <cecilemarcon02@gmail.com>
2020-12-06 15:03:57 +0100

    yo

--- gui.mli
+++
@@ -1,6 +0,0 @@
-type point = Geometrie.point
-
-val draw_path : point array -> unit
-val draw_poly : point array -> unit
-val draw_obstacles : point array array-> unit
-val create : point array array ->  point array -> unit
